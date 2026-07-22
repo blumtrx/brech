53657d13e588d0c7590560eb6462910c9572e6e7
blumtrx <o_shamim@ymail.com>
2026-07-22 10:13:30 -0400

    banner grab

---
+++ pysrc/tcpcon.py
@@ -0,0 +1,19 @@
+import socket
+
+print("Hello world")
+
+soc = socket.socket()
+
+host = input("Enter Host IP: ")
+port = int(input("Inpur port number: "))
+
+try:
+    soc.connect((host, port))
+    banner = soc.recv(1024)
+    print(banner.decode('utf-8', errors='ignore'))
+    
+except Exception as e:
+    print(f"Connection error: {e}")
+    
+finally:
+    soc.close()
